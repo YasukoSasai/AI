5d91968757d57628cbb891e202235414a9e8698e
Yasuko Sasai <>
2020-11-28 23:56:32 +0900

    学習のテクニック

--- learningAI/technique/weight_init_compare.py
+++ learningAI/technique/weight_init_compare.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-import os
 import sys
 sys.path.append('/Users/eb604/deep-learning-from-scratch-master')
 
@@ -23,8 +22,8 @@ max_iterations = 2000
 weight_init_types = {'std=0.01': 0.01, 'Xavier': 'sigmoid', 'He': 'relu'}
 optimizer = SGD(lr=0.01)
 
-networks = {}
-train_loss = {}
+networks = {} #各初期値に対するnetworkを格納するためのディクショナリ
+train_loss = {} #各NNに対する誤差のディクショナリ
 for key, weight_type in weight_init_types.items():
     networks[key] = MultiLayerNet(input_size=784, hidden_size_list=[100, 100, 100, 100],
                                   output_size=10, weight_init_std=weight_type)
@@ -32,7 +31,7 @@ for key, weight_type in weight_init_types.items():
 
 
 # 2:訓練の開始==========
-for i in range(max_iterations):
+for i in range(max_iterations): #2000回繰り返す
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
@@ -52,13 +51,24 @@ for i in range(max_iterations):
 
 
 # 3.グラフの描画==========
-markers = {'std=0.01': 'o', 'Xavier': 's', 'He': 'D'}
-x = np.arange(max_iterations)
-for key in weight_init_types.keys():
-    plt.plot(x, smooth_curve(train_loss[key]), marker=markers[key], markevery=100, label=key)
-plt.xlabel("iterations")
-plt.ylabel("loss")
-plt.ylim(0, 2.5)
-plt.legend()
+# markers = {'std=0.01': 'o', 'Xavier': 's', 'He': 'D'}
+# x = np.arange(max_iterations)
+# for key in weight_init_types.keys():
+#     plt.plot(x, smooth_curve(train_loss[key]), marker=markers[key], markevery=100, label=key)
+# plt.xlabel("iterations")
+# plt.ylabel("loss")
+# plt.ylim(0, 2.5)
+# plt.legend()
 # plt.show()
-print(networks)
+# print(networks)
+
+for i, a in activations.items():
+    plt.subplot(1, len(activations), i+1)
+    plt.title(str(i+1) + "-layer")
+    if i != 0: plt.yticks([], [])
+    # plt.xlim(0.1, 1)
+    # plt.ylim(0, 7000)
+    plt.hist(a.flatten(), 30, range=(0,1))
+# plt.show()
+
+
