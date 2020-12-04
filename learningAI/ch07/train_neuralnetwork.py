a041052641bcc71b9863037596bd6ee210700258
Yasuko Sasai <>
2020-12-05 01:10:49 +0900

    マルチレイヤーの修正

--- learningAI/ch07/train_neuralnetwork.py
+++ learningAI/ch07/train_neuralnetwork.py
@@ -19,18 +19,12 @@ iter_per_epoch = max(train_size / batch_size, 1) #60000/128 = エポック毎の
 
 
 # 1:最適化アルゴリズムの設定==========
-optimizers = {}
+#SGD, Momentum, AdaGrad, Adam, RMSpropに変えられるよ
+optimizers = Adam()
 
-optimizers['Adam'] = Adam()
-#optimizers['RMSprop'] = RMSprop()
-
-networks = {}
-train_loss = {}
-for key in optimizers.keys():
-    networks[key] = MultiLayerNet(
+networks = MultiLayerNet(
         input_size=784, hidden_size_list=[100, 100, 100, 100],
-        output_size=10)
-    train_loss[key] = []    
+        output_size=10)  
 
 train_loss_list = [] #学習ごとの損失関数を格納するためのリスト
 train_acc_list = [] #学習における正確率
@@ -43,25 +37,31 @@ for i in range(max_iterations): #2000回まで
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
     
-    for key in optimizers.keys(): #パラメータの最適化
-        grads = networks[key].gradient(x_batch, t_batch) #network.gradientから得た勾配
-        optimizers[key].update(networks[key].params, grads) #gradsを入れてoptimizer.updateを実行
     
-        loss = networks[key].loss(x_batch, t_batch) #network.lossを実行
-        train_loss[key].append(loss) #train_lossにlossを追加
+    grads = networks.gradient(x_batch, t_batch) #network.gradientから得た勾配
+    optimizers.update(networks.params, grads) #gradsを入れてoptimizer.updateを実行
+
+    loss = networks.loss(x_batch, t_batch) #network.lossを実行
+    train_loss_list.append(loss) #train_lossにlossを追加
     
-    if i % 100 == 0: #学習100回(128枚*100)ごとに精度を表示
+    if i % 100 == 0: #学習100回(128枚*100ごとに精度を表示
         print( "===========" + "iteration:" + str(i) + "===========")
-        for key in optimizers.keys():
-            loss = networks[key].loss(x_batch, t_batch)
-            print(key + ":" + str(loss))
+        loss = networks.loss(x_batch, t_batch)
+        print( "誤差:" + str(loss))
+    
+    if i % 100 == 0:
+        train_acc = networks.accuracy(x_train, t_train)
+        test_acc = networks.accuracy(x_test, t_test)
+        
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("訓練データの精度, テストデータの精度 | " + str(train_acc) + ", " + str(test_acc))
 
 
 # 3.誤差グラフの描画==========
-# markers = {"SGD": "o", "Momentum": "x", "AdaGrad": "s", "Adam": "D"}
 x = np.arange(max_iterations)
-for key in optimizers.keys():
-    plt.plot(x, smooth_curve(train_loss[key]), markevery=100, label=key)
+# for key in optimizers.keys():
+plt.plot(x, smooth_curve(train_loss_list), markevery=100, label="Adam")
 plt.xlabel("iterations")
 plt.ylabel("loss")
 plt.ylim(0, 1)
